bbb3e41e4010c56c4dc62d681b4f0e953c705333
maxjimenez7 <mrjimenez.410413@gmail.com>
2022-04-05 13:36:35 -0700

    Fixed navigation.xml design layout with correct destinations

--- app/src/main/java/com/example/bioplasticbad/HomepageFragment.kt
+++ app/src/main/java/com/example/bioplasticbad/HomepageFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [homepage.newInstance] factory method to
+ * Use the [HomepageFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class homepage : Fragment() {
+class HomepageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class homepage : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            homepage().apply {
+            HomepageFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
