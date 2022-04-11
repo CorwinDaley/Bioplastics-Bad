44ad3601b20850232111436a4df5ea76fd578b06
BBisharat4 <54417200+BBisharat4@users.noreply.github.com>
2022-04-11 14:13:26 -0700

    toast in main activity

--- app/src/main/java/com/example/bioplasticbad/MainActivity.kt
+++ app/src/main/java/com/example/bioplasticbad/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.bioplasticbad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val profilePageFragmentButton = findViewById<Button>(R.id.button_main_home)
+        profilePageFragmentButton.setOnClickListener {
+           Toast.makeText(this,"hello", Toast.LENGTH_LONG).show()
+        }
 
         var testAccount = account("JoneCat69","Password!","google.com/search?q=image_of_a_profile_picture")
         var testJournal = journal("2022.4.7","google.com/search?q=image_of_a_water_bottle","The water bottle is has begun degrading")
