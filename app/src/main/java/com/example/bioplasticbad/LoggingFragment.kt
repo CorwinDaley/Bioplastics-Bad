498bf4b482a22d2c996e00be5376bc943b2d9f90
girlinblack <phoebe.dickinson@gmail.com>
2022-04-15 14:26:50 -0700

    the problem with the main activity not showing the display is fixed not

--- app/src/main/java/com/example/bioplasticbad/LoggingFragment.kt
+++ app/src/main/java/com/example/bioplasticbad/LoggingFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.navigation.findNavController
 
 
