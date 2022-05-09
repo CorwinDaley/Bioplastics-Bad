21bafb561b63536afc6b1187e2f6d63e90200996
BBisharat4 <54417200+BBisharat4@users.noreply.github.com>
2022-05-09 13:48:45 -0700

    editprofile button makes name editable?

--- app/src/main/java/com/example/bioplasticbad/profilepage.kt
+++ app/src/main/java/com/example/bioplasticbad/profilepage.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,12 +33,22 @@ class profilepage : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater:LayoutInflater, container : ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_account_layout, container, false)
+
+        rootView.findViewById<EditText>(R.id.editText_accountLayout_name).isFocusable = false
+
+        val editProfile = rootView.findViewById<Button>(R.id.button_accountLayout_editProfile)
+        editProfile.setOnClickListener {
+            rootView.findViewById<EditText>(R.id.editText_accountLayout_name).isFocusable = true
+        }
+
         return inflater.inflate(R.layout.fragment_account_layout, container, false)
     }
+
 
     companion object {
         /**
