34c83f2425e36f3ca928f9b452f0dd2c87a03b5e
corwi <Corwin.Daley@gmail.com>
2022-05-19 12:51:04 -0700

    Started reworking the bottom navigation

--- app/src/main/java/com/example/bioplasticbad/MainActivity.kt
+++ app/src/main/java/com/example/bioplasticbad/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.bioplasticbad
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -11,6 +13,7 @@ import com.backendless.Backendless
 import com.backendless.BackendlessUser
 import com.backendless.async.callback.AsyncCallback
 import com.backendless.exceptions.BackendlessFault
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -29,53 +32,40 @@ class MainActivity : AppCompatActivity() {
             "B6BC75E8-E224-40E6-FFF3-0493FAE41F00",
             "2492C2DA-9DD5-436A-A97E-27B6DCAB1FFF" )
 
-        val loggingButton = findViewById<Button>(R.id.button_main_log)
-        loggingButton.setOnClickListener {
-            Toast.makeText(this, "Log button has been pressed", Toast.LENGTH_LONG).show()
+        val homepageFragment = R.layout.fragment_homepage
+        val profileFragment = R.layout.fragment_account_layout
+        val loggingFragment= R.layout.fragment_logging_layout
+        val instructionsFragment = R.layout.fragment_instructions
+
+
+
+        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+            bottomNav.setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.navigation_home->setCurrentFragment(homepageFragment)
+                    R.id.navigation_person->setCurrentFragment(profileFragment)
+                    R.id.navigation_add_circle->setCurrentFragment(loggingFragment)
+                    R.id.navigation_library_books->setCurrentFragment(instructionsFragment)
+                    R.id.navigation_store->Toast.LENGTH_LONG
+            }
+                true
         }
 
+
+
     }
+
+    private fun setCurrentFragment(newFragment: Fragment ) =
+        supportFragmentManager.beginTransaction().apply {
+        replace(R.layout.activity_main as Int, newFragment)
+            commit()
+        }
+
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
         return navController.navigateUp()
-    }
-
-
-
-    fun getAccount(username: String, password: String) {
-
-
-        // Double check for null usernames or passwords first please!
-
-        Backendless.UserService.login(
-            username,
-            password,
-
-            object : AsyncCallback<BackendlessUser?> {
-
-                override fun handleResponse(user: BackendlessUser?){
-                    // User has logged in
-                    var ownerId = user?.objectId
-                    var bundle = Bundle()
-                    bundle.putString(USER_ID_KEY,ownerId)
-
-                }
-
-                override fun handleFault(fault: BackendlessFault){
-                    // Error has occurred
-                }
-            }
-        )
-
-        //var account = Backendless.Data.of( Account::class.java ).findById(ownerId, object : AsyncCallback<Account> {
-        //    override fun handleResponse(response: Account?) {
-        //        TODO("Not yet implemented")
-        //    }
-        //
-        //    override fun handleFault(fault: BackendlessFault?) {
-        //        TODO("Not yet implemented")
-        //    }
-        //})
     }
 
 
