5f7b70a1710f830a997f1f92655fb4fc23e180fe
CorwinDaley <Corwin.Daley@gmail.com>
2022-05-19 13:41:32 -0700

    error hell

--- app/src/main/java/com/example/bioplasticbad/MainActivity.kt
+++ app/src/main/java/com/example/bioplasticbad/MainActivity.kt
@@ -6,13 +6,19 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.backendless.Backendless
 import com.backendless.BackendlessUser
 import com.backendless.async.callback.AsyncCallback
 import com.backendless.exceptions.BackendlessFault
+import com.example.bioplasticbad.databinding.ActivityMainBinding
+
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -22,46 +28,40 @@ class MainActivity : AppCompatActivity() {
         val USER_ID_KEY = "USERIDKEY"
     }
 
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Connect the backendless to our account
-        Backendless.initApp(this,
+        Backendless.initApp(
+            this,
             "B6BC75E8-E224-40E6-FFF3-0493FAE41F00",
-            "2492C2DA-9DD5-436A-A97E-27B6DCAB1FFF" )
+            "2492C2DA-9DD5-436A-A97E-27B6DCAB1FFF"
+        )
 
-        val homepageFragment = R.layout.fragment_homepage
-        val profileFragment = R.layout.fragment_account_layout
-        val loggingFragment= R.layout.fragment_logging_layout
-        val instructionsFragment = R.layout.fragment_instructions
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navView: BottomNavigationView = binding.bottomNavigationView
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+//        val navController = findNavController(R.id.myNavHostFragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homepage, R.id.profilepage, R.id.loggingFragment, R.id.instructionsFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
-
-        var bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-
-            bottomNav.setOnItemSelectedListener {
-                when(it.itemId){
-                    R.id.navigation_home->setCurrentFragment(homepageFragment)
-                    R.id.navigation_person->setCurrentFragment(profileFragment)
-                    R.id.navigation_add_circle->setCurrentFragment(loggingFragment)
-                    R.id.navigation_library_books->setCurrentFragment(instructionsFragment)
-                    R.id.navigation_store->Toast.LENGTH_LONG
-            }
-                true
-        }
-
-
-
+        navController.navigate(R.id.loginFragment)
     }
-
-    private fun setCurrentFragment(newFragment: Fragment ) =
-        supportFragmentManager.beginTransaction().apply {
-        replace(R.layout.activity_main as Int, newFragment)
-            commit()
-        }
-
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
